96ece214e21728edd4e29e784d4d91145394ce65
olvwl <syafridamelania@gmail.com>
2019-12-11 11:03:58 +0700

    caribobot

--- finalpemlan.c
+++ finalpemlan.c
@@ -190,3 +190,40 @@ void caritiket(){
 	if (awal > akhir)
 		printf("Data %d tidak ditemukan.\n", cari);
 }
+void caribobot(){
+	int cari;
+	int awal = 0;
+	int akhir = index_data;
+	int tengah = (awal+akhir)/2;
+ 	
+ 	printf("Masukkan bobot yang dicari : ");
+ 	scanf("%d",&cari);
+ 	
+	while(awal<=akhir)
+	{
+    	if (masuk[tengah].bobot < cari)
+    	{
+    		awal = tengah + 1;    
+    	}
+		else if (masuk[tengah].bobot == cari) 
+		{
+        	printf("=================================================\n");
+			printf("		Data Penumpang\n");
+			printf("=================================================\n");
+			printf("Nomor Tiket : %d\n", masuk[tengah].nomortiket);
+			printf("Tanggal Masuk : %d / %d  / %d\n", masuk[tengah].date.tgl, masuk[tengah].date.bln, masuk[tengah].date.thn);
+			printf("Nomor Kendaraan : %s \n", masuk[tengah].kendaraan.nopol);
+			printf("Tujuan : %s \n", masuk[tengah].tujuan);
+			printf("Jenis Kendaraan : %s\n", masuk[tengah].kendaraan.jenis);
+			printf("Bobot Kendaraan : %d ton\n\n\n", masuk[tengah].bobot);
+        	break;
+      	}
+      	else{
+        	akhir = tengah - 1;
+    	}
+    	tengah = (awal + akhir)/2;
+   }
+   
+	if (awal > akhir)
+		printf("Data %d tidak ditemukan.\n", cari);
+}
